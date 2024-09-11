f43fc9e5ed6bf56c2950296bf023550a999af1d1
m.biletskyi <m.biletskyi@levi9.com>
2024-09-11 20:51:10 +0300

    added serializers, urls

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from .serializers import CompanySerializer
+from .models import Company
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("-last_updated")
+    pagination_class = PageNumberPagination
+
