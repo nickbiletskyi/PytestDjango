e4b653a532d5f23e4a80b3fd9f2401968080a99d
m.biletskyi <m.biletskyi@levi9.com>
2024-09-20 14:46:19 +0300

    added locust test for fibonacci

---
+++ fibonacci/tests/locustfile.py
@@ -0,0 +1,16 @@
+from locust import HttpUser, between, task
+
+class FibonacciUser(HttpUser):
+    """
+    to run locust
+    locust -f locustfile.py --host http://127.0.0.1:8000
+    """
+    # Wait time between each request (in seconds)
+    wait_time = between(1, 2)
+
+    @task
+    def test_fibonacci(self):
+        # Simulate a user making a GET request to the Fibonacci endpoint
+        for n in range(1, 21):  # Testing Fibonacci for n from 1 to 20
+            self.client.get(f"/fibonacci/", params={"n": n})
+
