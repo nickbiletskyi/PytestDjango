395d72e259de8db5eccdb9784b24426649b4e703
m.biletskyi <m.biletskyi@levi9.com>
2024-09-25 13:58:57 +0300

    added pytest requests tests

---
+++ companies/tests/test_generic_api.py
@@ -0,0 +1,31 @@
+import requests
+
+
+import json
+
+
+companies_url = "http://127.0.0.1:8000/companies/"
+
+
+def test_zero_companies_should_return_empty_list() -> None:
+    response = requests.get(url=companies_url)
+    response_content = json.loads(response.content)
+    assert response.status_code == 200
+    assert response_content == []
+
+
+def test_create_company_with_only_name():
+    company_name = 'Google'
+    response = requests.post(url=companies_url, json={"name": company_name})
+    assert response.status_code == 201, f'Response Content: {response.content}'
+    response_content = json.loads(response.content)
+    assert response_content.get("name") == company_name
+    assert response_content.get("status") == "Hiring"
+    assert response_content.get("application_link") == ""
+    assert response_content.get("notes") == ""
+    delete_company(company_id=response_content['id'])
+
+
+def delete_company(company_id: str | int):
+    response = requests.delete(url=f'{companies_url}{company_id}/')
+    assert response.status_code == 204
