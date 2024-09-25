14cfce088ec77b2ad334e66f55cae9e72ea7a6c0
m.biletskyi <m.biletskyi@levi9.com>
2024-09-25 14:03:44 +0300

    skip requests tests

--- companies/tests/test_generic_api.py
+++ companies/tests/test_generic_api.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 
@@ -6,14 +7,14 @@ import json
 
 companies_url = "http://127.0.0.1:8000/companies/"
 
-
+@pytest.mark.skip_ci
 def test_zero_companies_should_return_empty_list() -> None:
     response = requests.get(url=companies_url)
     response_content = json.loads(response.content)
     assert response.status_code == 200
     assert response_content == []
 
-
+@pytest.mark.skip_ci
 def test_create_company_with_only_name():
     company_name = 'Google'
     response = requests.post(url=companies_url, json={"name": company_name})
@@ -24,7 +25,6 @@ def test_create_company_with_only_name():
     assert response_content.get("application_link") == ""
     assert response_content.get("notes") == ""
     delete_company(company_id=response_content['id'])
-
 
 def delete_company(company_id: str | int):
     response = requests.delete(url=f'{companies_url}{company_id}/')
