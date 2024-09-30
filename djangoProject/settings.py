6c8f030549ed463f162431e945ba34a7ddf4b936
m.biletskyi <m.biletskyi@levi9.com>
2024-09-30 19:59:36 +0300

    added dotenv

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,15 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-khip15l0)o%npd6sw+*jja71xxd1d&yv^pzdo-&$0(1!4nikn$"
-
+SECRET_KEY = os.getenv('SECRET')
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
+print(DEBUG)
 
-ALLOWED_HOSTS = ["pytest-django-bpdde2g9dpesehg9.polandcentral-01.azurewebsites.net"]
+ALLOWED_HOSTS = ["pytest-django-bpdde2g9dpesehg9.polandcentral-01.azurewebsites.net", "127.0.0.1", "localhost"]
 
 
-# Application definition
+# Application definitionR
 
 INSTALLED_APPS = [
     "django.contrib.admin",
